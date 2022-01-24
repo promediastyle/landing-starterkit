96921e4a2e50322be1bfe5012bb4680729b298bd
Promedia Style <91064803+promediastyle@users.noreply.github.com>
2022-01-24 20:47:30 +0300

    update

--- components/Modal/Form.vue
+++ components/Modal/Form.vue
@@ -1,8 +1,11 @@
 <template>
   <transition name="modal-fade">
     <div class="bg-black/75 fixed inset-0 flex justify-center items-baseline z-30 p-4 overflow-auto" @click="$emit('close-modal')">
-      <div class="w-full max-w-lg bg-white rounded-lg p-4 text-center" @click.stop>
+      <div class="w-full max-w-md bg-white rounded-lg p-4 text-center" @click.stop>
         <h2 class="mb-4 text-2xl font-bold">Modal Form</h2>
+
+        FORM
+
         <button class="py-2 px-6 bg-indigo-600 text-white rounded-full" @click="$emit('close-modal')">close</button>
       </div>
     </div>
@@ -17,5 +20,12 @@
 .modal-fade-enter-active,
 .modal-fade-leave-active {
   transition: opacity 0.5s ease;
+}
+
+.iframe {
+  border: none;
+  width: 100%;
+  height: 554px;
+  overflow: hidden;
 }
 </style>
