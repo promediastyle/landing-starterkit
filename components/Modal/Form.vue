38288b638af9742a30cb81df7b591b6e9fae9d6f
Promedia Style <91064803+promediastyle@users.noreply.github.com>
2022-01-24 20:01:25 +0300

    Add Modal Component

---
+++ components/Modal/Form.vue
@@ -0,0 +1,49 @@
+<template>
+  <transition name="modal-fade">
+    <div class="modal-overlay" @click="$emit('close-modal')">
+      <div class="modal" @click.stop>
+        <h2>Form</h2>
+
+        <button @click="$emit('close-modal')">close</button>
+      </div>
+      
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  background-color: #000000da;
+  overflow: hidden !important;
+}
+.modal {
+  text-align: center;
+  background-color: white;
+  height: 500px;
+  width: 500px;
+  margin-top: 10%;
+  padding: 60px 0;
+  border-radius: 20px;
+  overflow: hidden !important;
+}
+
+.modal-fade-enter,
+.modal-fade-leave-to {
+  opacity: 0;
+}
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,25 @@
     <main class="container p-4">
 
       Home
-
+      
       <div class="h-[1000px]"></div>
-      <h2 id="anchor">Anchor</h2>
+
+      <div id="anchor">
+        <button class="py-2 px-6 rounded-full bg-indigo-600 text-white" @click="showModal = true">Form</button>
+        <ModalForm v-show="showModal" @close-modal="showModal = false" class="z-50" />
+      </div>
+      
       <div class="h-[1000px]"></div>
     </main>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      showModal: false,
+    }
+  },
+}
+</script>
