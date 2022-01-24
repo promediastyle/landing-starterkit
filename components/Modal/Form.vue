fab0a7037753dc997b71f9fefe008febe2f09fc5
Promedia Style <91064803+promediastyle@users.noreply.github.com>
2022-01-24 20:34:30 +0300

    upd

--- components/Modal/Form.vue
+++ components/Modal/Form.vue
@@ -1,8 +1,8 @@
 <template>
   <transition name="modal-fade">
-    <div class="bg-black/75 fixed inset-0 flex justify-center items-center z-30" @click="$emit('close-modal')">
+    <div class="bg-black/75 fixed inset-0 flex justify-center items-baseline z-30 p-4 overflow-auto" @click="$emit('close-modal')">
       <div class="w-full max-w-lg bg-white rounded-lg p-4 text-center" @click.stop>
-        <h2 class="mb-4 text-2xl font-bold">Modal 1</h2>
+        <h2 class="mb-4 text-2xl font-bold">Modal Form</h2>
         <button class="py-2 px-6 bg-indigo-600 text-white rounded-full" @click="$emit('close-modal')">close</button>
       </div>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -8,10 +8,8 @@
       Home
 
       <div class="py-10">
-        <button class="py-2 px-6 rounded-full bg-indigo-600 text-white" @click="showModal = true">Modal 1</button>
-        <button class="py-2 px-6 rounded-full bg-indigo-600 text-white" @click="showModal2 = true">Modal 2</button>
+        <button class="py-2 px-6 rounded-full bg-indigo-600 text-white" @click="showModal = true">Modal Form</button>
         <ModalForm v-show="showModal" @close-modal="showModal = false" />
-        <ModalForm2 v-show="showModal2" @close-modal="showModal2 = false" />
       </div>
       
       <div class="h-[1000px]"></div>
@@ -26,7 +24,6 @@ export default {
   data() {
     return {
       showModal: false,
-      showModal2: false,
     }
   },
   watch: {
@@ -41,17 +38,6 @@ export default {
         }
       }
     },
-    showModal2: {
-      immediate: true,
-      handler(isOpen) {
-        if (process.client) {
-          if (isOpen)
-            document.body.style.setProperty("overflow", "hidden");
-          else
-            document.body.style.removeProperty("overflow");
-        }
-      }
-    }
   },
 }
 </script>
