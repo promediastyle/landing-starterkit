504f00863edb2f29ecd68885d83f8dd91a48f1cc
Promedia Style <91064803+promediastyle@users.noreply.github.com>
2022-01-24 20:47:47 +0300

    upd

--- components/Modal/Form.vue
+++ components/Modal/Form.vue
@@ -3,9 +3,6 @@
     <div class="bg-black/75 fixed inset-0 flex justify-center items-baseline z-30 p-4 overflow-auto" @click="$emit('close-modal')">
       <div class="w-full max-w-md bg-white rounded-lg p-4 text-center" @click.stop>
         <h2 class="mb-4 text-2xl font-bold">Modal Form</h2>
-
-        FORM
-
         <button class="py-2 px-6 bg-indigo-600 text-white rounded-full" @click="$emit('close-modal')">close</button>
       </div>
     </div>
